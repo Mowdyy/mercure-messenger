fb51f6d904d294ac180c5b9440b8d039abfc2cfd
Benoît Favrie <benoit14.favrie@gmail.com>
2022-11-22 09:59:37 +0100

    setup canceled

--- frontend/src/Hook/useGetUserList.js
+++
@@ -1,9 +0,0 @@
-export default function useGetUserList() {
-    return function () {
-        return fetch('http://localhost:8245/user-list', {
-            method: 'GET',
-            mode: "cors"
-        })
-            .then(data => data.json())
-    }
-}
