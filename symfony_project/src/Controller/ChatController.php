9f19500ed31788dffd16ae822505518cbbf084bb
Benoît Favrie <benoit14.favrie@gmail.com>
2022-11-23 16:02:30 +0100

    update chatcontroller

--- symfony_project/src/Controller/ChatController.php
+++ symfony_project/src/Controller/ChatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Mercure\HubInterface;
@@ -10,22 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ChatController extends AbstractController
 {
-    #[Route('/message-publish', name: 'message-publish')]
+    #[Route('/messages', name: 'messages', methods: 'GET')]
+    public function getAllMessages(MessageRepository $messageRepository): JsonResponse
+    {   
+        $messages = $messageRepository->findAll();
+        return $this->json($messages);
+    }
+
+    #[Route('/messages/add', name: 'messages-add', methods: 'POST')]
     public function index(HubInterface $hub): JsonResponse
     {
+        $newMessage = new Message();
         $update = new Update(
-            [
-                "https://example.com/my-private-topic"
-            ],
-            json_encode([
-                "message" => "Hello World!"
-            ])
+            ["https://example.com/my-private-topic"],
+            json_encode(["message" => "Hello World!"])
         );
 
         $hub->publish($update);
 
-        return $this->json([
-            'message' => 'Data published'
-        ]);
+        $this->json($newMessage);
+        return $this->redirectToRoute('messages');
     }
 }
