8c052384bd07664d10deb256f0173745bbb55007
Benoît Favrie <benoit14.favrie@gmail.com>
2022-11-25 09:15:56 +0100

    add import

--- symfony_project/src/Entity/Message.php
+++ symfony_project/src/Entity/Message.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MessageRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
